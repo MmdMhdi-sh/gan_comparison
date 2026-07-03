2050c4445f1d6aff5a08c6f9aefad5b6ccfa7114
Mohammad Mahdi <mmdmhdi.shhr@gmail.com>
2026-07-03 19:16:38 +0330

    Discriminator model implemented

---
+++ models/discriminator.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+class Maxout(nn.Module):
+    def __init__(self, in_features: int, out_features: int, pieces: int = 5):
+        super().__init__()
+        
+        self.in_features = in_features
+        self.out_features = out_features
+        self.pieces = pieces
+
+        self.linear = nn.Linear(
+            in_features=in_features,
+            out_features=out_features*pieces
+        )
+
+    def forward(self, x):
+        x = self.linear(x)
+        # (batch, out_features * pieces) -> (batch, out_features, pieces)
+        x = x.view(
+            x.size(0),
+            self.out_features,
+            self.pieces
+        )
+
+        return torch.max(x, dim=2).values
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            Maxout(784, 1024, 3),
+            nn.Dropout(0.5),
+
+            Maxout(1024, 512, 3),
+            nn.Dropout(0.5),
+
+            Maxout(512, 256, 3),
+            nn.Dropout(0.5),
+
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+
+        return self.network(x)
+    
--- models/generator.py
+++ models/generator.py
@@ -1,48 +1,26 @@
-import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.config = config
         self.latent_dim = config["latent_dim"]
         self.network = nn.Sequential(
-            nn.Linear(
-                in_features=self.latent_dim,
-                out_features=256
-            ),
+            nn.Linear(self.latent_dim, 256),
             nn.ReLU(inplace=True),
-            nn.Linear(
-                in_features=256,
-                out_features=512
-            ),
+
+            nn.Linear(256, 512),
             nn.ReLU(inplace=True),
-            nn.Linear(
-                in_features=512,
-                out_features=1024
-            ),
+
+            nn.Linear(512, 1024),
             nn.ReLU(inplace=True),
-            nn.Linear(
-                in_features=1024,
-                out_features=784
-            ),
+
+            nn.Linear(1024, 784),
             nn.Sigmoid()
         )
     
     def forward(self, z):
+        # (batch_size, latent_dim) -> (batch_size, 1, 28, 28)
         x = self.network(z)
         x = x.view(-1, 1, 28, 28)
         return x
     
-
-config = {
-    "latent_dim": 100
-}
-
-generator = Generator(config=config)
-
-z = torch.randn(16, 100)
-
-fake_images = generator(z)
-
-print(fake_images.shape)
